df346448d43822dc84416e379074f5f902542f58
Gašper Šavle <126724362+gaspersavle@users.noreply.github.com>
2024-05-15 12:04:30 +0200

    Added option to controll both grippers at the same time

--- gripper_driver/gpio_client.py
+++ gripper_driver/gpio_client.py
@@ -3,12 +3,16 @@ import sys
 from std_srvs.srv import SetBool
 from rclpy.node import Node
 
-class GPIOControlClient:
+GRIPPER_L = 'gripper_left'
+GRIPPER_R = 'gripper_right'
+
+class GPIOControlClient(Node):
     def __init__(self):
         super().__init__('gpio_control_client')
-        self.client = self.create_client(SetBool, 'gpio_control')
+        self.client = self.create_client(SetBool, GRIPPER_L)
+        self.client = self.create_client(SetBool, GRIPER_R)
 
-    def toggle_gpio_pin(self, pin_state):
+    def write_gpio_pin_left(self, pin_state):
         request = SetBool.Request()
         request.data = pin_state
 
@@ -17,19 +21,60 @@ class GPIOControlClient:
 
         future = self.client.call_async(request)
         rclpy.spin_until_future_complete(self, future)
-
+ 
         if future.result() is not None:
             self.node.get_logger().info('GPIO pin toggled successfully')
         else:
             self.node.get_logger().error('Failed to toggle GPIO pin')
+                                         
+    def write_gpio_pin_right(self, pin_state):   
+        request = SetBool.Request()
+        request.data = pin_state
+ 
+        while not self.client.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('Service not available, waiting again...')
+ 
+        future = self.client.call_async(request)
+        rclpy.spin_until_future_complete(self, future)
+ 
+        if future.result() is not None:
+            self.node.get_logger().info('GPIO pin toggled successfully')
+        else:
+            self.node.get_logger().error('Failed to toggle GPIO pin')
+ 
+    def write_gpio_pins(self, pin_state):        
+        request = SetBool.Request()
+        request.data = pin_state     
+                                                                         
+        while not self.client.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('Service not available, waiting again...')
+                    
+        future = self.client.call_async(request)
+        rclpy.spin_until_future_complete(self, future)
+          
+        if future.result() is not None:
+            self.node.get_logger().info('GPIO pin toggled successfully')
+        else:                                                                                                                                                                                                                                 
+            self.node.get_logger().error('Failed to toggle GPIO pin') 
 
-def main():
-    rclpy.init()
-    gpio_client = GPIOControlClient()
-    pin_state = True  # Set to True to turn on the pin, False to turn off
-    gpio_client.toggle_gpio_pin(pin_state)
-    gpio_client.node.destroy_node()
-    rclpy.shutdown()
+    def main():
+        rclpy.init()
+        gpio_client = GPIOControlClient()
+    
+        grpper = input("Enter gripper you want to control (left, right, both): ")
+        assert gripper in ['left', 'right', 'both'], f"{Fore.RED}Invalid gripper name: {gripper}, should be one of: [left, right, both]{Fore.RESET}"
+    
+        state = int(input("Enter desired state [0 (closed), 1(opened)]: "))
+        assert state in [0, 1], f"{Fore.RED}Invalid state: {state}, should be one of: [0, 1]{Fore.RESET}"
+    
+        if gripper == "left":
+            gpio_client.write_gpio_pin_left(state)
+        elif gripper == "right":
+            gpio_client.write_gpio_pin_right(state)
+        else:
+            gpio_client.write_gpio_pins(state)
+        gpio_client.destroy_node()
+        rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
